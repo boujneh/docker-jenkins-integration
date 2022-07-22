bec5e29d3bb131b1b7318d9f5e04d8e7ae4501f9
boujneh <houssemboujneh@gmail.com>
2022-07-23 01:43:28 +0200

    first commit

---
+++ src/test/java/com/boujneh/dockerjenkinsintegration/DockerJenkinsIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boujneh.dockerjenkinsintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerJenkinsIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
